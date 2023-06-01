32b5987ac7369afd9abdd470d01fe7bb47cc809b
tuana <tuananh45468@gmail.com>
2023-06-02 00:19:34 +0700

    middleware

---
+++ middleware.js
@@ -0,0 +1,42 @@
+const { NextResponse } = require("next/server");
+const { jwtVerify } = require("jose");
+const PUBLIC_FILE = /\.(.*)$/;
+
+const verifyJWT = async (jwt) => {
+  const { payload } = await jwtVerify(
+    jwt,
+    new TextEncoder().encode(process.env.JWT_SECRET)
+  );
+
+  return payload;
+};
+
+export default async function middleware(req, res) {
+  const { pathname } = req.nextUrl;
+  if (
+    pathname.startsWith("/_next") ||
+    pathname.startsWith("/api") ||
+    pathname.startsWith("/static") ||
+    pathname.startsWith("/login") ||
+    pathname.startsWith("/signup") ||
+    PUBLIC_FILE.test(pathname)
+  ) {
+    return NextResponse.next();
+  }
+
+  const jwt = req.cookies.get(process.env.COOKIE_NAME);
+
+  if (!jwt) {
+    req.nextUrl.pathname = "/login";
+    return NextResponse.redirect(req.nextUrl);
+  }
+
+  try {
+    await verifyJWT(jwt.value);
+    return NextResponse.next();
+  } catch (e) {
+    console.error(e);
+    req.nextUrl.pathname = "/login";
+    return NextResponse.redirect(req.nextUrl);
+  }
+}
